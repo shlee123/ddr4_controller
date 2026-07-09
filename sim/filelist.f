cbd44a19edf3632f7773a59a588f27c0957b95c5
shlee123 <37898869+shlee123@users.noreply.github.com>
2026-07-10 01:57:43 +0800

    V2.1: use consolidated controller top in simulation filelist

--- sim/filelist.f
+++ sim/filelist.f
@@ -1,6 +1,6 @@
-../rtl/ddr4_pkg.sv
+../rtl/pkg/ddr4_ctrl_pkg.sv
 ../rtl/sync_fifo.sv
 ../rtl/async_fifo.sv
-../rtl/ddr4_ctrl_top.sv
+../rtl/ddr4_controller_top.sv
 ./model/ddr4_sdram_model.sv
 ./tb/ddr4_ctrl_tb.sv
