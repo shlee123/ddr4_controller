246bb773f5df986a6bce46305ce74de2b4cbeb94
shlee123 <37898869+shlee123@users.noreply.github.com>
2026-07-17 00:39:42 +0800

    ci: add production RTL compile filelist

---
+++ sim/filelist/rtl_production.f
@@ -0,0 +1,10 @@
++incdir+../../rtl/pkg
+../../rtl/pkg/ddr4_ctrl_pkg.sv
+../../rtl/sync_fifo.sv
+../../rtl/async_fifo.sv
+../../rtl/ddr4_data_cache.sv
+../../rtl/ddr4_scheduler.sv
+../../rtl/phy/ddr4_fpga_clockgen.sv
+../../rtl/phy/ddr4_ck_out.sv
+../../rtl/phy/ddr4_dq_dqs_phy.sv
+../../rtl/ddr4_controller_top.sv
