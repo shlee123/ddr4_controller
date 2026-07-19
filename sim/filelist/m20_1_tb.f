68cb10a2d09b7f7acd6b0c19cacc36c6584b89da
shlee123 <37898869+shlee123@users.noreply.github.com>
2026-07-19 22:37:04 +0800

    M20.1 add integration filelist

---
+++ sim/filelist/m20_1_tb.f
@@ -0,0 +1,5 @@
+-f ../filelist/rtl_production.f
+../../rtl/core/ddr4_axi_burst_engine.sv
+../../rtl/core/ddr4_perf_monitor.sv
+../../rtl/ddr4_controller_top_m20_1.sv
+../../sim/tb/production/tb_ddr4_controller_m20_1.sv
