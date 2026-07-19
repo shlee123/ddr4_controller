936613a46e28944d0a6804f98305df4a94608a5b
shlee123 <37898869+shlee123@users.noreply.github.com>
2026-07-19 21:48:43 +0800

    M10: add closure regression file list

---
+++ sim/filelist/m10_tb.f
@@ -0,0 +1,5 @@
+../../rtl/ddr4_timing_refresh_guard.sv
+../../rtl/ddr4_bank_scheduler.sv
+../../rtl/phy/ddr4_phy_training.sv
+../../rtl/ddr4_verification_counters.sv
+../tb/production/tb_ddr4_controller_m10.sv
