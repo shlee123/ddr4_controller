dd7cd6af2b538f3f55ea89da2ea4f7388df443e9
shlee123 <37898869+shlee123@users.noreply.github.com>
2026-07-22 08:33:33 +0800

    M33 add regression filelist

---
+++ sim/filelist/m33_tb.f
@@ -0,0 +1,6 @@
++incdir+../../rtl/pkg
+../../rtl/pkg/ddr4_ctrl_pkg.sv
+../../rtl/core/ddr4_address_mapper.sv
+../../rtl/phy/ddr4_x16_data_path.sv
+../../rtl/core/ddr4_scheduler_open_page.sv
+../tb/production/tb_ddr4_controller_m33.sv
